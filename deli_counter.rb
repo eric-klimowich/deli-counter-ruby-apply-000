5aadad6af534bda4c48042fb0c56ec1f69dff014
Eric Klimowich <eklimowich@hotmail.com>
2018-08-12 06:45:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,3 +18,11 @@ def take_a_number(katz_deli, customer)
   puts "Welcome, #{customer}. You are number #{katz_deli.length} in line."
 end
 
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
